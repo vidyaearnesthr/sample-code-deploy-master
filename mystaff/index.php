39ca03c34a464a7022c4f5300e2222e9a2822b0f
vidya game <vidya.game@earnesthr.com>
2017-04-24 12:30:23 +0530

    Update index.php

--- mystaff/index.php
+++ mystaff/index.php
@@ -1,11 +1,11 @@
 <?php
 
-$conn =mysqli_connect($_SERVER['dbinstance.c96uwmzzvktk.us-west-2.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['root1234'], $_SERVER['testdb'], $_SERVER['3306']);
+$conn =mysql_connect($_SERVER['dbinstance.c96uwmzzvktk.us-west-2.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['root1234'], $_SERVER['testdb'], $_SERVER['3306']);
 if(! $conn )
 {
-  die('Could not connect: ' . mysqli_error());
+  die('Could not connect: ' . mysql_error());
 }
 
 echo "Fetched data successfully\n";
-mysqli_close($conn);
+mysql_close($conn);
 ?>
