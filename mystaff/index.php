61f574d14ac624aabd77723085b302e7a143f80d
vidya game <vidya.game@earnesthr.com>
2017-04-24 12:22:14 +0530

    Update index.php

--- mystaff/index.php
+++ mystaff/index.php
@@ -2,7 +2,7 @@
 $dbhost = '';
 $dbuser = 'root';
 $dbpass = 'root1234';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+$conn = $conn = mysqli_connect($_SERVER['dbinstance.c96uwmzzvktk.us-west-2.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['root1234'], $_SERVER['testdb'], $_SERVER['3306']);
 if(! $conn )
 {
   die('Could not connect: ' . mysql_error());
