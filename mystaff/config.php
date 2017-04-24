7567728ae504a4f44dc06941526a906555ea4365
vidya game <vidya.game@earnesthr.com>
2017-04-24 17:36:07 +0530

    Update config.php

--- mystaff/config.php
+++ mystaff/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysql_connect($_SERVER['dbinstance.c96uwmzzvktk.us-west-2.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['root1234'], $_SERVER['testdb']);
+$conn = mysql_connect($_SERVER['dbinstance.c96uwmzzvktk.us-west-2.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['root1234']);
 if(! $conn )
 {
   die('Could not connect: ' . mysql_error());
