7818c2dba329655ba2a87f6ac7d1ebcdfd4a1dec
vidya game <vidya.game@earnesthr.com>
2017-04-24 15:57:45 +0530

    Create config.php

---
+++ mystaff/config.php
@@ -0,0 +1,25 @@
+<?php
+$conn = mysql_connect($_SERVER['dbinstance.c96uwmzzvktk.us-west-2.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['root1234'], $_SERVER['testdb']);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+mysql_select_db('testdb',$conn);
+$sql = 'SELECT * FROM `testdb`.`logTable`';
+
+
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not get data: ' . mysql_error());
+}
+while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+{
+    echo "Tutorial ID :{$row['id']}  <br> ".
+         "UserName: {$row['userName']} <br> ".
+         "Password: {$row['password']} <br> ".
+          "--------------------------------<br>";
+} 
+echo "Fetched data successfully\n";
+mysql_close($conn);
+?>
