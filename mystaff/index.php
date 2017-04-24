07d53b861bfbccfbcf7fb948f876aaee9413b349
vidya game <vidya.game@earnesthr.com>
2017-04-24 12:27:20 +0530

    Update index.php

--- mystaff/index.php
+++ mystaff/index.php
@@ -1,28 +1,11 @@
 <?php
-$dbhost = '';
-$dbuser = 'root';
-$dbpass = 'root1234';
+
 $conn =mysqli_connect($_SERVER['dbinstance.c96uwmzzvktk.us-west-2.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['root1234'], $_SERVER['testdb'], $_SERVER['3306']);
 if(! $conn )
 {
-  die('Could not connect: ' . mysql_error());
+  die('Could not connect: ' . mysqli_error());
 }
-$sql = 'SELECT id,userName, 
-               password FROM logTable';
 
-mysql_select_db('testdb');
-$retval = mysql_query( $sql, $conn );
-if(! $retval )
-{
-  die('Could not get data: ' . mysql_error());
-}
-while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
-{
-    echo "Tutorial ID :{$row['id']}  <br> ".
-         "UserName: {$row['userName']} <br> ".
-         "Password: {$row['password']} <br> ".
-          "--------------------------------<br>";
-} 
 echo "Fetched data successfully\n";
-mysql_close($conn);
+mysqli_close($conn);
 ?>
