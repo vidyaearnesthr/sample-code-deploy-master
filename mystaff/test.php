e6a60189660a3e25cb2098547700cabef6dfa9fa
vidya game <vidya.game@earnesthr.com>
2017-05-02 15:57:53 +0530

    Update test.php

--- mystaff/test.php
+++ mystaff/test.php
@@ -1,4 +1,8 @@
 <?php
+
+phpinfo();
+var_dump(function_exists('mysqli_connect'));
+/*
 echo "Welcome<br/>";
 $servername = "mystaffdev-test.cthwf1dxttem.ap-southeast-1.rds.amazonaws.com";
 $username = "dev";
@@ -6,6 +10,9 @@ $password = "rdsdevtest";
 $dbname="mystaff";
 echo "variables set </br>";
 // Create connection
+phpinfo();
+var_dump(function_exists('mysqli_connect'));
+die();
 if(function_exists(mysqli))
 {
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -23,4 +30,5 @@ if ($conn->connect_error) {
   {
    echo "functionf not exist";
   }
+  */
 ?>
