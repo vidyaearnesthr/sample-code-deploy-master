3ac7073e4746283077873f883eeec788f685ce42
vidya game <vidya.game@earnesthr.com>
2017-05-02 15:32:11 +0530

    update test

--- mystaff/test.php
+++ mystaff/test.php
@@ -1,3 +1,8 @@
 <?php
-echo "Welcome";
+echo "Welcome<br/>";
+$servername = "mystaffdev-test.cthwf1dxttem.ap-southeast-1.rds.amazonaws.com";
+$username = "dev";
+$password = "rdsdevtest";
+$dbname="mystaff";
+echo "variables set </br>";
 ?>
