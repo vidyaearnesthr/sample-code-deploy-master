6f07cc91f5e427a010f6b10e8d807123ac854028
vidya game <vidya.game@earnesthr.com>
2017-05-02 15:35:10 +0530

    c

--- mystaff/test.php
+++ mystaff/test.php
@@ -5,4 +5,15 @@ $username = "dev";
 $password = "rdsdevtest";
 $dbname="mystaff";
 echo "variables set </br>";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+echo "Connection Created </br>";
+// Check connection
+if ($conn->connect_error) {
+	echo "Not Connected </br>";
+    echo "Connection failed: " . $conn->connect_error;
+}else
+{
+	echo "Connected;
+  }
 ?>
